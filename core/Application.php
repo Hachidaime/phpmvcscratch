36be8b6d138004f0e5e2f9f82b1d2c84db973327
Hachidaime <cold.kid40@gmail.com>
2020-11-29 06:02:54 +0700

    Implement Views & Layouts

--- core/Application.php
+++ core/Application.php
@@ -10,18 +10,24 @@ namespace app\core;
  */
 class Application
 {
+    public static string $ROOT_DIR;
     public Router $router;
     public Request $request;
+    public Response $response;
+    public static Application $app;
 
     /**
      * Application __construct
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($rootPath)
     {
+        self::$ROOT_DIR = $rootPath;
+        self::$app = $this;
         $this->request = new Request();
-        $this->router = new Router($this->request);
+        $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
     }
 
     /**
@@ -31,7 +37,7 @@ class Application
      */
     public function run()
     {
-        $this->router->resolve();
+        echo $this->router->resolve();
     }
 }
 ?>
--- core/Router.php
+++ core/Router.php
@@ -12,16 +12,20 @@ namespace app\core;
 class Router
 {
     public Request $request;
+    public Response $response;
     protected array $routes = [];
 
     /**
-     * Router constructor.
+     * __construct
      *
-     * @param app\core\Request $request
+     * @param app/core/Request $request
+     * @param app/core/Response $response
+     * @return void
      */
-    public function __construct(\app\core\Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     /**
@@ -37,6 +41,18 @@ class Router
     }
 
     /**
+     * post
+     *
+     * @param  mixed $path
+     * @param  mixed $collback
+     * @return void
+     */
+    public function post($path, $collback)
+    {
+        $this->routes['post'][$path] = $collback;
+    }
+
+    /**
      * resolve
      *
      * @return void
@@ -48,11 +64,59 @@ class Router
         $callback = $this->routes[$method][$path] ?? false;
 
         if ($callback === false) {
-            echo 'Not found';
-            exit();
+            $this->response->setStatusCode(404);
+            return $this->renderView('_404');
         }
 
-        echo call_user_func($callback);
+        if (is_string($callback)) {
+            return $this->renderView($callback);
+        }
+
+        return call_user_func($callback);
+    }
+
+    /**
+     * renderView
+     *
+     * @param  mixed $view
+     * @return void
+     */
+    public function renderView($view)
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{ content }}', $viewContent, $layoutContent);
+    }
+
+    public function renderContent($viewContent)
+    {
+        $layoutContent = $this->layoutContent();
+        return str_replace('{{ content }}', $viewContent, $layoutContent);
+    }
+
+    /**
+     * layoutContent
+     *
+     * @return void
+     */
+    protected function layoutContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . '/views/layout/main.tpl';
+        return ob_get_clean();
+    }
+
+    /**
+     * renderOnlyView
+     *
+     * @param  mixed $view
+     * @return void
+     */
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/{$view}.tpl";
+        return ob_get_clean();
     }
 }
 ?>
